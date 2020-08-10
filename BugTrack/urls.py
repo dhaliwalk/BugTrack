3c6e5cc9d4ff6c72db4b27c1d5896a65c03bfe75
dhaliwalk <kevindhal@gmail.com>
2020-08-10 06:43:15 -0700

    Added a new base template from getbootstrap

--- BugTrack/urls.py
+++ BugTrack/urls.py
@@ -22,12 +22,12 @@ from django.conf.urls.static import static
 from users.views import TeamCreateView
 
 urlpatterns = [
+    path('', include('main.urls')),
     path('admin/', admin.site.urls),
     path('register/', user_views.register, name='register'),
     path('profile/', user_views.profile, name='profile'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
-    path('', include('main.urls')),
     path('team/', include('users.urls')),
     path('project/', include('projects.urls')),
     path('team/new/', TeamCreateView.as_view(), name='team-create'),
