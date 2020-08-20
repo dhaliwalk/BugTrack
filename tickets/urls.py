d4e0f60c91737a7b35b793733a62e93b3cd26ba8
dhaliwalk <kevindhal@gmail.com>
2020-08-20 16:43:51 -0700

    Added update and delete functions for tickets and comments

--- tickets/urls.py
+++ tickets/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
 from . import views
-from .views import TicketCreateView, CommentCreateView
+from .views import TicketCreateView, TicketUpdateView, TicketDeleteView, CommentCreateView, CommentUpdateView, CommentDeleteView
 
 urlpatterns = [
     path('<int:pk>/info/', views.TicketInfo, name='ticket-info'),
     path('<int:pk>/new/', TicketCreateView.as_view(), name='ticket-create'),
+    path('<int:pk>/update/', TicketUpdateView.as_view(), name='ticket-update'),
+    path('<int:pk>/delete/', TicketDeleteView.as_view(), name='ticket-delete'),
     path('<int:pk>/comment/new/', CommentCreateView.as_view(), name='comment-create'),
+    path('<int:pk>/comment/update/', CommentUpdateView.as_view(), name='comment-update'),
+    path('<int:pk>/comment/delete/', CommentDeleteView.as_view(), name='comment-delete'),
 ]
--- tickets/views.py
+++ tickets/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from .models import Ticket, Comment 
 from projects.models import Project
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
-from django.views.generic import CreateView
+from django.views.generic import CreateView, UpdateView, DeleteView
 
 def TicketInfo(request, pk=None):
 	if pk:
@@ -21,6 +21,29 @@ class TicketCreateView(LoginRequiredMixin, CreateView):
 		form.instance.submitter = self.request.user
 		return super().form_valid(form)
 
+class TicketUpdateView(UserPassesTestMixin, LoginRequiredMixin, UpdateView):
+	model = Ticket
+	fields = ['title', 'description', 'priority', 'status', 'ticket_type']
+
+	def form_valid(self, form):
+		form.instance.submitter = self.request.user
+		return super().form_valid(form)
+
+	def test_func(self):
+		ticket = self.get_object()
+		if self.request.user == ticket.submitter:
+			return True
+		return False
+
+class TicketDeleteView(UserPassesTestMixin, LoginRequiredMixin, DeleteView):
+	model = Ticket
+	success_url = "/"
+	def test_func(self):
+		ticket = self.get_object()
+		if self.request.user == ticket.submitter:
+			return True
+		return False
+
 class CommentCreateView(LoginRequiredMixin, CreateView):
 	model = Comment
 	fields = ['message']
@@ -30,3 +53,30 @@ class CommentCreateView(LoginRequiredMixin, CreateView):
 		form.instance.ticket = Ticket.objects.get(pk=pk)
 		form.instance.author = self.request.user
 		return super().form_valid(form)
+
+class CommentUpdateView(UserPassesTestMixin, LoginRequiredMixin, UpdateView):
+	model = Comment
+	fields = ['message']
+
+	def form_valid(self, form):
+		form.instance.author = self.request.user
+		return super().form_valid(form)
+
+	def test_func(self):
+		comment = self.get_object()
+		if self.request.user == comment.author:
+			return True
+		return False
+
+class CommentDeleteView(UserPassesTestMixin, LoginRequiredMixin, DeleteView):
+	model = Comment
+	success_url = "/"
+	def test_func(self):
+		comment = self.get_object()
+		if self.request.user == comment.author:
+			return True
+		return False
+
+
+
+
